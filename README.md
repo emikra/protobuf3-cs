16b769051aaab7b9519630c64cfa6f05bf2654f1
Craig Gabbadon <craig.gabbadon@emikra.com>
2016-04-29 15:27:30 -0400

    Initial release with .NET 3.5 compatibility

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-protobuf3
+# protobuf 3 for .NET Framework 3.5
+
+This is a _use-at-your-own-risk_ fork of the Protocol Buffers C# implementation for .NET 3.5 framework compatibility. The project builds the `Google.Protobuf.dll` library. It is based on the [**v3.0.0-beta-2**](https://github.com/google/protobuf/releases/tag/v3.0.0-beta-2) release from the [official project](https://github.com/google/protobuf) from Google.
---
+++ protobuf3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Google.Protobuf")]
+[assembly: AssemblyDescription("Protocol Buffers 3 implementation for C#")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Google.Protobuf")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f9d77b8-fa5e-4f1a-b3e7-03442b8ea892")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
